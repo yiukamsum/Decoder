616e72d6b7461556f42d96b9bf50090c651eda4f
yiukamsum <yiukamsum@outlook.com>
2018-05-30 00:30:41 +0800

    Testing MD5

--- decoder.py
+++ decoder.py
@@ -47,8 +47,8 @@ def caesar_cipher():
                 continue
 
 
-for abc in range(0, 25):
-    print("+" + str(abc + 1) + "\t" + new[abc].replace("\n", "\n\t"))
+    for abc in range(0, 25):
+        print("+" + str(abc + 1) + "\t" + new[abc].replace("\n", "\n\t"))
     main()
 
 def base64():
@@ -64,6 +64,38 @@ def main():
             break
         if case('2'):
             caesar_cipher()
+            break
+        if case('3'):
+            import hashlib
+            import sys
+            import time
+            def timing(f):
+                def wrap(*args):
+                    time1 = time.time()
+                    ret = f(*args)
+                    time2 = time.time()
+                    print("%s Time: %0.3f s" % (f.func_name, float(time2 - time1)))
+                    return ret
+                return wrap
+            @timing
+            def decryptMD5(testHash):
+                s = []
+                while True:
+                    m = hashlib.md5()
+                    for c in s:
+                        m.update(chr(c).encode("utf-8"))
+                    hash = m.hexdigest()
+                    if hash == testHash:
+                        return ''.join([chr(c) for c in s])
+                    wrapped = True
+                    for i in range(0, len(s)):
+                        s[i] = (s[i] + 1) % 256
+                        if s[i] != 0:
+                            wrapped = False
+                            break
+                    if wrapped:
+                        s.append(0)
+            print(decryptMD5("9743a66f914cc249efca164485a19c5c".encode("utf-8")))
             break
         if case(): # default, could also just omit condition or 'if True'
             print("Error!")
